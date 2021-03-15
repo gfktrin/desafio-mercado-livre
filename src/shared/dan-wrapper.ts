1a58600f52f03a8a1f99dfe270d854c321876a8c
Gabriel Trin <gfktrin@yahoo.com.br>
2021-03-14 23:11:56 -0300

    update wrapper and service

--- src/shared/dan-wrapper.ts
+++ src/shared/dan-wrapper.ts
@@ -5,32 +5,30 @@ export class DanWrapper {
   //   url = 'https://www.dan.me.uk/tornodes';
   url = 'http://127.0.0.1:8080';
 
-  getIps() {
+  async getIps() {
     const ipList = [];
     let start = false;
 
-    return axios.get(this.url).then(
-      (response) => {
-        if (response.status === 200) {
-          const html = response.data;
-          const $ = cheerio.load(html);
-          $('div.article.box')
-            .contents()
-            .each(function () {
-              if (this.nodeType === 8) {
-                start = !start;
-              }
-              if (start && this.nodeType === 3) {
-                const value = this.nodeValue.split('|')[0].replace('\n', '');
-                if (value != '') {
-                  ipList.push(value);
-                }
-              }
-            });
-          return ipList;
-        }
-      },
-      (error) => console.log(error),
-    );
+    const response = await axios.get(this.url);
+
+    if (response.status === 200) {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      $('div.article.box')
+        .contents()
+        .each(function () {
+          if (this.nodeType === 8) {
+            start = !start;
+          }
+          if (start && this.nodeType === 3) {
+            const value = this.nodeValue.split('|')[0].replace('\n', '');
+            if (value != '') {
+              ipList.push(value);
+            }
+          }
+        });
+      return ipList;
+    }
+    return ipList;
   }
 }
--- src/tor-node/tor-node.service.ts
+++ src/tor-node/tor-node.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { DanWrapper } from 'src/shared/dan-wrapper';
+import { Sources } from 'src/shared/enums/sources.enum';
 import { TorNode } from './tor-node.model';
 
 @Injectable()
@@ -12,6 +13,36 @@ export class TorNodeService {
 
   async getIps() {
     const dan = new DanWrapper();
-    return dan.getIps();
+    const ipList = await dan.getIps();
+    for (const ip of ipList) {
+      const torNode = await this.getByIp(ip);
+      if (torNode) {
+        torNode.updatedAt = new Date();
+        torNode.source = Sources.dan;
+        await this.torNodeModel.updateOne({ _id: torNode.id }, torNode).exec();
+      } else {
+        const node = new this.torNodeModel({ ip, source: Sources.dan });
+        await node.save();
+      }
+    }
+    return ipList;
+  }
+
+  async getByIp(ip: string) {
+    return await this.torNodeModel.findOne({ ip }).exec();
+  }
+
+  async getById(id: string) {
+    return await this.torNodeModel.findById(id).exec();
+  }
+
+  async create(torNode: TorNode) {
+    const createdNode = new this.torNodeModel(torNode);
+    return await createdNode.save();
+  }
+
+  async update(id: string, node: TorNode) {
+    await this.torNodeModel.updateOne({ _id: id }, node).exec();
+    return this.getById(id);
   }
 }
